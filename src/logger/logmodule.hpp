2e5f560c13529faf74178d9fa1fb25def73205a8
Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>
2024-11-08 14:39:57 +0200

    [logger] Add logger
    Signed-off-by: Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>

---
+++ src/logger/logmodule.hpp
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2024 Renesas Electronics Corporation.
+ * Copyright (C) 2024 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef LOGMODULE_HPP_
+#define LOGMODULE_HPP_
+
+#include <sstream>
+
+#include <aos/common/tools/log.hpp>
+
+#ifndef LOG_MODULE
+#define LOG_MODULE "default"
+#endif
+
+#define LOG_DBG() LOG_MODULE_DBG(LOG_MODULE)
+#define LOG_INF() LOG_MODULE_INF(LOG_MODULE)
+#define LOG_WRN() LOG_MODULE_WRN(LOG_MODULE)
+#define LOG_ERR() LOG_MODULE_ERR(LOG_MODULE)
+
+#endif
