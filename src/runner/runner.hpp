e4c183979f430632052c91455d110fe00c93dd91
Mykola Kobets <mykola_kobets@epam.com>
2024-12-24 15:35:41 +0200

    [runner] Implement runner
    Signed-off-by: Mykola Kobets <mykola_kobets@epam.com>

--- src/runner/runner.hpp
+++ src/runner/runner.hpp
@@ -7,46 +7,105 @@
 #ifndef RUNNER_HPP_
 #define RUNNER_HPP_
 
-#include <aos/sm/runner.hpp>
+#include <chrono>
+#include <condition_variable>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <string>
+#include <thread>
+
+#include "aos/common/tools/time.hpp"
+#include "aos/common/types.hpp"
+#include "systemdconn.hpp"
 
 namespace aos::sm::runner {
 
 /**
- * Runner interface.
+ * Service runner.
  */
 class Runner : public RunnerItf {
 public:
     /**
-     * Starts instance.
+     * Initializes Runner instance.
+     *
+     * @param receiver run status receiver.
+     * @return Error.
+     */
+    Error Init(RunStatusReceiverItf& receiver);
+
+    /**
+     * Starts monitoring thread.
+     *
+     * @return Error.
+     */
+    Error Start();
+
+    /**
+     * Stops Runner.
+     *
+     * @return Error.
+     */
+    Error Stop();
+
+    /**
+     * Destructor.
+     */
+    ~Runner();
+
+    /**
+     * Starts service instance.
      *
      * @param instanceID instance ID.
      * @param runtimeDir directory with runtime spec.
      * @param runParams runtime parameters.
      * @return RunStatus.
      */
-    RunStatus StartInstance(const String& instanceID, const String& runtimeDir, const RunParameters& runParams) override
-    {
-        (void)instanceID;
-        (void)runtimeDir;
-        (void)runParams;
-
-        return {"", InstanceRunStateEnum::eActive, ErrorEnum::eNone};
-    }
+    RunStatus StartInstance(const String& instanceID, const String& runtimeDir, const RunParameters& params) override;
 
     /**
-     * Stops instance.
+     * Stops service instance.
      *
-     * @param instanceID instance ID>
+     * @param instanceID instance ID
      * @return Error.
      */
-    Error StopInstance(const String& instanceID) override
-    {
-        (void)instanceID;
+    Error StopInstance(const String& instanceID) override;
 
-        return ErrorEnum::eNone;
-    }
+private:
+    static constexpr auto cDefaultStartInterval   = 5 * Time::cSeconds;
+    static constexpr auto cDefaultStopTimeout     = 5 * Time::cSeconds;
+    static constexpr auto cStartTimeMultiplier    = 1.2;
+    static constexpr auto cDefaultStartBurst      = 3;
+    static constexpr auto cDefaultRestartInterval = 1 * Time::cSeconds;
+
+    static constexpr auto cStatusPollPeriod = std::chrono::seconds(1);
+
+    static constexpr auto cSystemdUnitNameTemplate = "aos-service@%s.service";
+    static constexpr auto cSystemdDropInsDir       = "/run/systemd/system";
+    static constexpr auto cParametersFileName      = "parameters.conf";
+
+    virtual std::shared_ptr<SystemdConnItf> CreateSystemdConn();
+    virtual std::string                     GetSystemdDropInsDir() const;
+
+    void             MonitorUnits();
+    Array<RunStatus> GetRunningInstances() const;
+    Error            SetRunParameters(const String& unitName, const RunParameters& params);
+    Error            RemoveRunParameters(const String& unitName);
+    std::string      CreateSystemdUnitName(const String& instance);
+
+    RunStatusReceiverItf* mRunStatusReceiver = nullptr;
+
+    std::shared_ptr<SystemdConnItf> mSystemd;
+    std::thread                     mMonitoringThread;
+    std::mutex                      mMutex;
+    std::condition_variable         mCondVar;
+
+    std::map<std::string, InstanceRunState> mRunningUnits;
+    mutable std::vector<RunStatus>          mRunningInstances;
+
+    bool mClosed = false;
 };
 
 } // namespace aos::sm::runner
 
-#endif
+#endif // RUNNER_HPP_
